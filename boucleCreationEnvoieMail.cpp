6022539cf8fce89b33a724f358c4b0fabb14bf74
jcanto <jessy.canto.sio@gmail.com>
2015-10-02 14:03:25 +0200

    Update boucleCreationEnvoieMail.cpp
    **

--- boucleCreationEnvoieMail.cpp
+++ boucleCreationEnvoieMail.cpp
@@ -1,1 +1,30 @@
+#include "boucleCreationEnvoieMail.h"
 
+
+BoucleCreationEnvoieMail::BoucleCreationEnvoieMail()
+{
+    QSqlQuery requeteUti;
+    requeteUti.exec("select distinct utiNum from pointRelaisParUtilisateur");
+    int numUti=0;
+    while(requeteUti.next());
+    {
+        QString utiActu = requeteUti.value(numUti).toString();
+        QSqlQuery requeteRel;
+        requeteRel.exec("select relNum from pointRelaisParUtilisateur where utiNum="+utiActu);
+        QString relChaine = "catalogueNewWorld";
+        int relActu = 0;
+        while(requeteRel.next())
+        {
+            relChaine = relChaine + "-" + requeteRel.value(relActu).toString();
+            relActu=relActu+1;
+        }
+        relChaine = relChaine + ".pdf";
+        QFile pdfFichier(relChaine);
+        if(!(pdfFichier.exists()))
+        {
+            CreationPdf(*relChaine);
+        }
+        numUti=numUti+1;
+
+    }
+}
