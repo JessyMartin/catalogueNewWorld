c51cd365951704bf140ae969c6901edd02b8af40
jcanto <jessy.canto.sio@gmail.com>
2015-10-02 14:02:26 +0200

    Update boucleCreationEnvoieMail.h
    Déclaration

--- boucleCreationEnvoieMail.h
+++ boucleCreationEnvoieMail.h
@@ -1,1 +1,30 @@
+#ifndef BOUCLECREATIONENVOIEMAIL_H
+#define BOUCLECREATIONENVOIEMAIL_H
 
+#include "creationpdf.h"
+
+#include <QSqlDatabase>
+#include <QCoreApplication>
+#include <QSqlQuery>
+#include <QSqlRecord>
+#include <iostream>
+#include <QApplication>
+#include <QSqlDatabase>
+#include <iostream>
+#include <QtGui/QPrinter>
+#include <QtGui/QPainter>
+#include <QtGui>
+#include <QtCore>
+
+
+class BoucleCreationEnvoieMail
+{
+public:
+    BoucleCreationEnvoieMail();
+private:
+    int numUti;
+};
+
+
+
+#endif // BOUCLECREATIONENVOIEMAIL_H
